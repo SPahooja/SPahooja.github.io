d26e23bf0b0ca9251763cd8560400eeb1b97a4a5
Sahil Pahooja <pahoojasahil@gmail.com>
2020-06-10 17:22:29 +0530

    Completed Navbar, Hero and About

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+});
