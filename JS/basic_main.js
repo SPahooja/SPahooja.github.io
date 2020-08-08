7763794b6fe4af58e1b27f8976d7a22b45829b55
SPahooja <pahoojasahil@gmail.com>
2020-08-08 11:43:09 +0530

    Finalised the look, need to work on content

--- JS/basic_main.js
+++ JS/basic_main.js
@@ -28,4 +28,17 @@ $(document).ready(function() {
 
         $('.blurred-image').css('opacity', opacityVal);
     });
+    // show portfolio item extra content
+    $(".expandDetails").on("click", function() {
+        $(this).parent().siblings(".portfolio-long").slideToggle(500);
+        $(this).children(".arrowDown").toggle();
+        $(this).children(".arrowUp").toggle();
+    });
+    /*$(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.icon-scroll').fadeOut();
+        } else {
+            $('.icon-scroll').fadeIn();
+        }
+    });*/
 });
